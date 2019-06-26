018e0ec7a235066a486d7af074d18a07395f5433
lawtancool <26829131+lawtancool@users.noreply.github.com>
2019-06-25 20:23:07 -0700

    copy initial-config if config dir is empty

--- apache-proxy-start.sh
+++ apache-proxy-start.sh
@@ -5,6 +5,13 @@
 #Copy virtualhost on apache directory
 #cp /opt/proxy-conf/*.conf /etc/apache2/sites-available/
 
+if [ -z "$(ls -A /etc/apache2)" ]; then
+   echo "Config directory is empty, copying initial config..."
+   cp -r /etc/apache2-initial-config /etc/apache2
+else
+   echo "Config directory is not empty, continuing..."
+fi
+
 #List site and enable
 ls /etc/apache2/sites-available/ -1A | a2ensite *.conf
 
